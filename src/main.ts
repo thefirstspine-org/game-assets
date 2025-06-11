a5557894d6a1a70a636d29ac508cba1a32d40e5e
TheFirstSpine <teddy@thefirstspine.fr>
2025-06-11 13:37:00 +0200

    feat(assets): add static assets

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorFilter, LogsService, RequestsLoggerMiddleware } from '@thefirstspine/logs-nest';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   // Load dotenv config
   require('dotenv').config();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalFilters(new ErrorFilter(new LogsService()));
   app.use(RequestsLoggerMiddleware.use);
   await app.listen(process.env.PORT);
